f2efc09cc9baf08e38d39322f3140e39a574fe22
beth billings <bethfbillings@gmail.com>
2019-09-03 10:51:31 -0500

    Last last minute changes

--- P1/P1.Client/Controllers/Register.cs
+++ P1/P1.Client/Controllers/Register.cs
@@ -1,25 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
+using P1.Data.Entities;
 using P1.Domain.Models;
 
 namespace P1.Client.Controllers
 {
     public class Register : Controller
     {
+      P1.Client.Models.Register r = new P1.Client.Models.Register();
+      private readonly p1Context _db = new p1Context();
+      
+      [HttpGet]
       public IActionResult Index()
       {
         return View("Register");
       }
 
-      
-      public void RegisterNewUser()
+      [HttpPost]
+      public IActionResult Index(string newEmail, string newPassword, string fname, string lname)
       {
-        User u = new User
-        (
-          "{Email}",
-          "{Password}",
-          new Name("{fname}", "{lname}"),
-          new Order()
-        );
+        RegisterNewUser(newEmail, newPassword, fname, lname);
+        return View("Register");
+      }
+      
+      public void RegisterNewUser(string newEmail, string newPassword, string fname, string lname)
+      {
+        _db.User.Add(new Data.Entities.User
+        {
+          Email = newEmail,
+          Password = newPassword,
+          Name = new Data.Entities.Name{
+            FirstName = fname, 
+            LastName = lname } 
+        });
+        _db.SaveChanges();
       }
     }
 }
---
+++ P1/P1.Client/Models/Register.cs
@@ -0,0 +1,10 @@
+namespace P1.Client.Models
+{
+    public class Register
+    {
+      public string email;
+      public string password;
+      public string fname;
+      public string lname;
+    }
+}
